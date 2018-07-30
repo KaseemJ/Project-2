debd9b8bbd752e935d2ac6e9ab6aa6b06b04604f
Mark Faulkner <markfaulk350@gmail.com>
2018-07-30 14:14:48 -0700

    worked on routing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,21 +2,17 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/home", function(req, res) {
+    db.items.findAll({limit:20}).then(function(result) {
+      res.json(result);
     });
+    
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/:category", function(req, res) {
+    db.items.findOne({ where: { item_category: req.params.category } }).then(function(result) {
+      res.json(result);
     });
   });
 
