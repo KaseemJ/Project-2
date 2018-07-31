dbd9570dfce21cbcfcc2483fa6383d05cbc6775c
Mark Faulkner <markfaulk350@gmail.com>
2018-07-31 10:58:15 -0700

    added the navbar and took out the seuelize timestamp to make adding data easier

--- models/users.js
+++ models/users.js
@@ -1,11 +1,13 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var users = sequelize.define("users", {
     user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     user_name: DataTypes.STRING,
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     phone_number: DataTypes.STRING
-  });
+  },
+    {
+      timestamps: false
+    });
   return users;
 };
- 
