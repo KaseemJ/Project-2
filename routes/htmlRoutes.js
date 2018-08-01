38f3dae7e25206c187555a9a6eca4c4bbb5c5a39
wesley9mm <info.developer4@gmail.com>
2018-08-01 00:05:03 -0700

    page 2

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,11 +13,11 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/:category", function(req, res) {
-    db.items.findOne({ where: { item_category: req.params.category }}).then(function(result) {
-      res.json(result);
-    });
-  });
+  // app.get("/:category", function(req, res) {
+  //   db.items.findOne({ where: { itemCategory: req.params.category } }).then(function (result) {
+  //     res.json(result);
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
